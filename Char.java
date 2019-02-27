99b9e522d7d57a0e80d2f4c710e5619af59d74fe
Sathish3006 <45841493+Sathish3006@users.noreply.github.com>
2019-02-27 11:21:57 +0530

    Create Char.java

---
+++ Char.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Char{
+  public static void main(String[] args){
+    Scanner sc  = new Scanner(System.in);
+    String s = sc.next();
+    int nn = sc.nextInt();
+    String sc = s.substring(s.length()-nn,s.length());
+    System.out.println(sc);
+  }
+}
